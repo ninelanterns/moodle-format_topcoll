b04d66acd5d9d8e0d9f0fe323f472b3066cbabf1
Lucas Hau Dai <lucas.haudai@kineo.com.au>
2017-09-27 10:33:54 +1000

    HOMHAS-100 Adding style.css for plugin as it is not automatically added

--- lib.php
+++ lib.php
@@ -1175,8 +1175,9 @@ class format_topcoll extends format_base {
 
     //HOMHAS-100 Add boostrap for topcoll plugin
     public function page_set_course(moodle_page $page) {
-        $page->requires->css('/course/format/bootstrap/bootstrap_min.css');
-        $page->requires->js('/course/format/bootstrap/bootstrap.js');
+        $page->requires->css('/course/format/topcoll/bootstrap/bootstrap_min.css');
+        $page->requires->css('/course/format/topcoll/styles.css');
+        $page->requires->js('/course/format/topcoll/bootstrap/bootstrap.js');
     }
 }
 
